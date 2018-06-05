d4af05d29ff2eb3c7664a746de0e7b0266a10c4c
KelliePetersen <KellieAnnPetersen@gmail.com>
2018-06-05 16:04:31 -0400

    add jquery version

--- Basic Version/main.js
+++ Basic Version/main.js
@@ -1,7 +1,24 @@
-const li = document.getElementsByTagName('li');
+$("input[type='text']").hide();
 
-li.addEventListener('click', deleteTodo);
+$("ul").on("click", "li", function() {
+	$(this).toggleClass("completed");
+});
 
-function deleteTodo() {
-  
-}
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500,function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</i></span> " + todoText + "</li>");
+	}
+});
+
+$(".toggle-add").click(function() {
+	$("input[type='text']").fadeToggle();
+});
